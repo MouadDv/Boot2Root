959ae7a51d2051b4f7ce3fa11bc29d565afd0503
MouadDv <mouaddv@gmail.com>
2025-01-05 07:07:57 +0100

    Last modifications

--- scripts/generatepasswordlist.cpp
+++ scripts/generatepasswordlist.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 // OPEKMA,OPEKMQ,OPEKMa,OPEKMq,OPEKmA,OPEKmQ,OPEKma,OPEKmq,OPEkMA,OPEkMQ,OPEkMa,OPEkMq,OPEkmA,OPEkmQ,OPEkma,OPEkmq,OPUKMA,OPUKMQ,OPUKMa,OPUKMq,OPUKmA,OPUKmQ,OPUKma,OPUKmq,OPUkMA,OPUkMQ,OPUkMa,OPUkMq,OPUkmA,OPUkmQ,OPUkma,OPUkmq,OPeKMA,OPeKMQ,OPeKMa,OPeKMq,OPeKmA,OPeKmQ,OPeKma,OPeKmq,OPekMA,OPekMQ,OPekMa,OPekMq,OPekmA,OPekmQ,OPekma,OPekmq,OPuKMA,OPuKMQ,OPuKMa,OPuKMq,OPuKmA,OPuKmQ,OPuKma,OPuKmq,OPukMA,OPukMQ,OPukMa,OPukMq,OPukmA,OPukmQ,OPukma,OPukmq,OpEKMA,OpEKMQ,OpEKMa,OpEKMq,OpEKmA,OpEKmQ,OpEKma,OpEKmq,OpEkMA,OpEkMQ,OpEkMa,OpEkMq,OpEkmA,OpEkmQ,OpEkma,OpEkmq,OpUKMA,OpUKMQ,OpUKMa,OpUKMq,OpUKmA,OpUKmQ,OpUKma,OpUKmq,OpUkMA,OpUkMQ,OpUkMa,OpUkMq,OpUkmA,OpUkmQ,OpUkma,OpUkmq,OpeKMA,OpeKMQ,OpeKMa,OpeKMq,OpeKmA,OpeKmQ,OpeKma,OpeKmq,OpekMA,OpekMQ,OpekMa,OpekMq,OpekmA,OpekmQ,Opekma,Opekmq,OpuKMA,OpuKMQ,OpuKMa,OpuKMq,OpuKmA,OpuKmQ,OpuKma,OpuKmq,OpukMA,OpukMQ,OpukMa,OpukMq,OpukmA,OpukmQ,Opukma,Opukmq,oPEKMA,oPEKMQ,oPEKMa,oPEKMq,oPEKmA,oPEKmQ,oPEKma,oPEKmq,oPEkMA,oPEkMQ,oPEkMa,oPEkMq,oPEkmA,oPEkmQ,oPEkma,oPEkmq,oPUKMA,oPUKMQ,oPUKMa,oPUKMq,oPUKmA,oPUKmQ,oPUKma,oPUKmq,oPUkMA,oPUkMQ,oPUkMa,oPUkMq,oPUkmA,oPUkmQ,oPUkma,oPUkmq,oPeKMA,oPeKMQ,oPeKMa,oPeKMq,oPeKmA,oPeKmQ,oPeKma,oPeKmq,oPekMA,oPekMQ,oPekMa,oPekMq,oPekmA,oPekmQ,oPekma,oPekmq,oPuKMA,oPuKMQ,oPuKMa,oPuKMq,oPuKmA,oPuKmQ,oPuKma,oPuKmq,oPukMA,oPukMQ,oPukMa,oPukMq,oPukmA,oPukmQ,oPukma,oPukmq,opEKMA,opEKMQ,opEKMa,opEKMq,opEKmA,opEKmQ,opEKma,opEKmq,opEkMA,opEkMQ,opEkMa,opEkMq,opEkmA,opEkmQ,opEkma,opEkmq,opUKMA,opUKMQ,opUKMa,opUKMq,opUKmA,opUKmQ,opUKma,opUKmq,opUkMA,opUkMQ,opUkMa,opUkMq,opUkmA,opUkmQ,opUkma,opUkmq,opeKMA,opeKMQ,opeKMa,opeKMq,opeKmA,opeKmQ,opeKma,opeKmq,opekMA,opekMQ,opekMa,opekMq,opekmA,opekmQ,opekma,opekmq,opuKMA,opuKMQ,opuKMa,opuKMq,opuKmA,opuKmQ,opuKma,opuKmq,opukMA,opukMQ,opukMa,opukMq,opukmA,opukmQ,opukma,opukmq
 
-// 426315
+// 426135
 
 
 void generatePasswords(const vector<vector<string>>& combinations) {
@@ -37,7 +37,7 @@ void generatePasswords(const vector<vector<string>>& combinations) {
 
 
 int main() {
-    vector<vector<string>> combinations(6);  // A vector of 3 parts (arrays of strings)
+    vector<vector<string>> combinations(6);
 
     cout << "Enter combinations for part 1 (comma-separated): ";
     string input;
